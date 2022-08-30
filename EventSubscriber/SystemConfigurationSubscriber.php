81593810440d93b4730b58abae75481b0fc134f6
Kevin Papst <kpapst@gmx.net>
2022-08-30 18:33:48 +0200

    re-use invoice configuration section

--- EventSubscriber/SystemConfigurationSubscriber.php
+++ EventSubscriber/SystemConfigurationSubscriber.php
@@ -26,16 +26,19 @@ class SystemConfigurationSubscriber implements EventSubscriberInterface
      */
     public function onSystemConfiguration(SystemConfigurationEvent $event)
     {
-        $event->addConfiguration((new SystemConfigurationModel())
-            ->setSection('small_business_rule')
-            ->setConfiguration([
+        foreach ($event->getConfigurations() as $configuration) {
+            if ($configuration->getSection() !== 'invoice') {
+                continue;
+            }
+
+            $configuration->addConfiguration(
                 (new Configuration())
                     ->setName('small_business_rule.enable')
                     ->setLabel('small_business_rule.enable')
                     ->setRequired(false)
                     ->setType(YesNoType::class)
-                    ->setTranslationDomain('system-configuration'),
-            ])
-        );
+                    ->setTranslationDomain('system-configuration')
+            );
+        }
     }
 }
